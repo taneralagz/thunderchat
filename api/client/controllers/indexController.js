0b8c40813d6b9920e333abfc938d3b76e9ae3ecf
DenizhanSarac <78859953+DenizhanSarac@users.noreply.github.com>
2022-08-09 14:59:17 +0300

    test user fixlendi

--- api/client/controllers/indexController.js
+++ api/client/controllers/indexController.js
@@ -1,14 +1,18 @@
 module.exports = function(app){
-
-    app.get('/', function(req, res){
-        res.render('index');
-    });
-
-    app.get('/contact', function(req, res){
-        res.render('contact');
-    });
-
-    app.get('/about', function(req, res){
-        res.render('about');
-    });
+    try {
+        app.get('/', function(req, res){
+            res.render('index');
+        });
+    
+        app.get('/contact', function(req, res){
+            res.render('contact');
+        });
+    
+        app.get('/about', function(req, res){
+            res.render('about');
+        });
+    } catch (error) {
+        console.log("Something crashed."+ error.log)
+    }
+    
 }
